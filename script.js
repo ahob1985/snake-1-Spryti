0e88d6c709b904006aed28f06a174f0e0759c9a5
Spryti <67284103+Spryti@users.noreply.github.com>
2021-06-17 17:21:49 +0000

    Doing the body

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-// Author:
+// Author: Jordan Muturi
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
+let buttonDiv;
+let resetButton
 
 // Global Game Variables
-//let snake;
+let snake;
+let food;
+let resolution;
+let scaledwidth;
+let scaledheight;
+let score;
 
 function setup() {
   // Build the interface
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP();
+  textP.parent(textDiv);
+  buttonDiv = createDiv();
+  resetButton = createButton("Reset Game");
+  resetButton.mousePressed(resetGame);
+  resetButton.parent(buttonDiv);
   // Set the resolution to 20. Play with this later if you want.
-
+  resolution = 20;
   // Scaled width and height are width / resolution, height / resolution
+  scaledwidth = floor(width / resolution);
+  scaledheight = floor(height / resolution)
 
   // Set the game's framerate to 5 (or whatever you prefer)
-
+  framerate(5);
   // Call resetGame() to initialize everything else.
-
+  resetGame();
 }
 
 function draw() {
